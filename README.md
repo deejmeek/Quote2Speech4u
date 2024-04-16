e0e3540651f126eb6a59803ec7e7cb1bd7ae5387
DJMeekGit <d.j.meek@outlook.com>
2024-04-17 08:59:57 +1000

    removeEventListener on divTog

--- README.md
+++ README.md
@@ -21,3 +21,34 @@ Need to initiate quote api to call and display quoite text
 then need to active quote to speech api.
 
 then once t2s is initated investigate whether i can link conditions to the api deactivating or activating.
+
+////
+todos-
+
+Prevent click default when function is already running.
+
+get author data into drop down.
+
+media buttons
+
+styling
+
+github pages
+
+user friendly README.md
+
+issues -
+Programming quote API all API returns 501, need to use differnt API
+
+15 second limit
+
+///
+
+// var mycars = ['Herr','Frau'];
+// var list = document.getElementById('anrede');
+
+// mycars.forEach(function(item){
+// var option = document.createElement('option');
+// option.value = item;
+// list.appendChild(option);
+// });
--- index.js
+++ index.js
@@ -1,47 +1,61 @@
 const divTog = document.querySelector("div");
-const quoteP = document.getElementById("quote");
-const authorP = document.getElementById("author")
-console.log(authorP)
-console.log(quoteP)
+const quoteP = document.getElementById("quoteP");
+const authorP = document.getElementById("authorP");
+
+
+
 // api - key - https://api.quotable.io // https://quote-garden.onrender.com/api/v3/quotes
 // console.log(divTog)
 
+//https://programming-quotesapi.vercel.app/api/available
+//todo-  integrate category seach bar. - filter search dropdown
 
-//todo-  get t2p to toggle class
+
+
+
 
 divTog.addEventListener("click", async function fetchData() {
 
-   speechSynthesis.cancel();
-   const res = await fetch("https://programming-quotesapi.vercel.app/api/random");
+//    speechSynthesis.cancel();
+//    const res = await fetch("https://programming-quotesapi.vercel.app/api/random");
+   const res = await fetch("https://api.quotable.io/random");
 const data = await res.json();
-quoteP.innerHTML = data.quote;
-authorP.innerHTML = data.author;
 
 
-   let msg = data.quote;
-   const utterance = new SpeechSynthesisUtterance(msg);
-   speechSynthesis.speak(utterance);
+authorP.textContent = data.author;
+quoteP.textContent = data.content;
+
+let msg = data.content;
+const utterance = new SpeechSynthesisUtterance(msg);
+speechSynthesis.speak(utterance);
    
-
-
-utterance.addEventListener("end", (event) => {
-divTog.classList.remove('active')
+    utterance.addEventListener("end", (event) => {
+        // preventDefault();
+        divTog.classList.remove('active')
+        divTog.addEventListener("click", fetchData)
 
 })
   
-   
-   
-    divTog.classList.add('active')
-        // setTimeout(() => {
-        //     
-
-        //     },1000)
+divTog.classList.add('active')
+divTog.removeEventListener("click", fetchData);
+    
+    return;
+});
 
 
-return;
-        });
+        
 
+// divTog.addEventListener("click", async function fetchData(){
+// const res =  await fetch("https://programming-quotesapi.vercel.app/api/random")
+// const data = await res.json();
 
+// authorP.textContent = data.author;
+
+// console.log(data.author);
+
+// return;
+
+// });
 
 // fetchData();
 
@@ -56,7 +70,33 @@ return;
 
 
 
+// async function fetchAuthor(){
+// const res =  await fetch("https://api.quotable.io/authors")
+// const data = await res.json();
 
+// console.log(data)
+// let myAuth = data.name;
+
+// const sectors = ["Teddy","Buzz","Bob" ]
+// const input = document.getElementById('dropdown1')
+// const dataList = document.getElementById('author')
+
+// myAuth.forEach(function(item){
+//     let option = document.createElement('option');
+//     option.value = item;
+//     list.appendChild(option);
+
+
+
+
+
+// });
+
+
+
+// }
+
+// fetchAuthor();
 
 
 
