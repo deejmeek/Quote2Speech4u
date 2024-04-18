045fc3d126236cd02de20b373c811e893b65befa
DJMeekGit <d.j.meek@outlook.com>
2024-04-18 11:28:21 +1000

    Refactor js, refactor click eventlistener logic, added HTML favourite btns

--- index.js
+++ index.js
@@ -1,130 +1,40 @@
 const divTog = document.querySelector("div");
 const quoteP = document.getElementById("quoteP");
 const authorP = document.getElementById("authorP");
+const pitchUp = document.getElementById("pitchUp")
+const favouriteH3 = document.getElementById("favouriteH3")
+const favBtn = document.getElementById("favButton");
 
+let clickEnabled = true; 
 
+divTog.addEventListener("click", fetchData);
 
-// api - key - https://api.quotable.io // https://quote-garden.onrender.com/api/v3/quotes
-// console.log(divTog)
+async function fetchData() {
+    if (!clickEnabled) return; 
+    clickEnabled = false; 
 
-//https://programming-quotesapi.vercel.app/api/available
-//todo-  integrate category seach bar. - filter search dropdown
+    speechSynthesis.cancel();
+    const res = await fetch("https://api.quotable.io/random?maxLength=200");
+    const data = await res.json();
+    displayQuote(data);
+    speakNow(data);
 
+    divTog.classList.add('active');
+}
 
+function displayQuote(data) {
+    authorP.textContent = data.author;
+    quoteP.textContent = data.content;
+}
 
+function speakNow(data) {
+    let msg = data.content;
+    const utterance = new SpeechSynthesisUtterance(msg);
+    speechSynthesis.speak(utterance);
+    utterance.addEventListener("end", toggleWithDataOnEnd);
+}
 
-
-divTog.addEventListener("click", async function fetchData() {
-
-//    speechSynthesis.cancel();
-//    const res = await fetch("https://programming-quotesapi.vercel.app/api/random");
-   const res = await fetch("https://api.quotable.io/random");
-const data = await res.json();
-
-
-authorP.textContent = data.author;
-quoteP.textContent = data.content;
-
-let msg = data.content;
-const utterance = new SpeechSynthesisUtterance(msg);
-speechSynthesis.speak(utterance);
-   
-    utterance.addEventListener("end", (event) => {
-        // preventDefault();
-        divTog.classList.remove('active')
-        divTog.addEventListener("click", fetchData)
-
-})
-  
-divTog.classList.add('active')
-divTog.removeEventListener("click", fetchData);
-    
-    return;
-});
-
-
-        
-
-// divTog.addEventListener("click", async function fetchData(){
-// const res =  await fetch("https://programming-quotesapi.vercel.app/api/random")
-// const data = await res.json();
-
-// authorP.textContent = data.author;
-
-// console.log(data.author);
-
-// return;
-
-// });
-
-// fetchData();
-
-// document.getElementById("btn")
-// .addEventListener("click", () => {
-
-// var msg = "glad to meet you friend";
-// const utterance = new SpeechSynthesisUtterance(msg);
-// speechSynthesis.speak(utterance);
-
-// })
-
-
-
-// async function fetchAuthor(){
-// const res =  await fetch("https://api.quotable.io/authors")
-// const data = await res.json();
-
-// console.log(data)
-// let myAuth = data.name;
-
-// const sectors = ["Teddy","Buzz","Bob" ]
-// const input = document.getElementById('dropdown1')
-// const dataList = document.getElementById('author')
-
-// myAuth.forEach(function(item){
-//     let option = document.createElement('option');
-//     option.value = item;
-//     list.appendChild(option);
-
-
-
-
-
-// });
-
-
-
-// }
-
-// fetchAuthor();
-
-
-
-// divTog.addEventListener("click", function () {
-
-//     // divTog.classList.add('active')?.classList.remove('active');
-
-//     divTog.classList.add('active')
-// setTimeout(() => {
-
-// divTog.classList.remove('active')
-
-// },1000)
-
-// });
-
-
-// async function fetchData (){
-
-// const res = await fetch("https://programming-quotesapi.vercel.app/api/random");
-// const data = await res.json();
-// quoteP.innerHTML = data.quote;
-// authorP.innerHTML = data.author;
-// return;
-
-
-// }
-
-
-// fetchData();
-
+function toggleWithDataOnEnd(event) {
+    divTog.classList.remove('active');
+    clickEnabled = true; 
+}
